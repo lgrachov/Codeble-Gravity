e85e085e23b5d65f87c6a73407e76814a3a09323
lgrachov <78234184+lgrachov@users.noreply.github.com>
2021-12-10 22:29:36 +0200

    Наша команда для гравитации
    Option в начале надо

---
+++ gravity.py
@@ -0,0 +1,13 @@
+ for
+  import Cocoa, 
+  open(PlayerMove)
+ let Gravity64 = 5
+ var gravity64 = Move?,
+  
+  let Gravity64 = Player Moved?
+ if PLayer Moved
+  var Player = Gravity,
+ esle
+let MagicLive = 9 += 5NM
+ end,
+  var Player = gravity64
